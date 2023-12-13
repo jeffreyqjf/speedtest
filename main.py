f0fd6825c3145eddee2a530b11f9c8b80e31196a
jeffreyqjf <3172268428@qq.com>
2023-12-13 18:34:50 +0800

    finish web server

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import speedtest
 import sqlite3
 import time
 import requests
-from fake_useragent import UserAgent
+import sys
 
 
 "注意要关闭代理，并且确保没有网速占用(如下载资源)"
@@ -10,17 +10,25 @@ from fake_useragent import UserAgent
 place = input("please input the place num:")
 
 
+def error(e):
+    print("!!!")
+    print(e)
+    print("出现错误，敲回车键结束程序")
+    end = input()
+    exit(1)
+
+
 def get_time():
     time_ymd = time.strftime("%Y-%m-%d", time.gmtime())
     time_hms = time.strftime("%H:%M:%S", time.localtime())
-    weeks = time.strftime("%w", time.gmtime())
-    print("现在时间：", time_ymd, time_hms, "星期：", weeks)
-    return time_ymd, time_hms, weeks
+    week = time.strftime("%w", time.gmtime())
+    print("现在时间：", time_ymd, time_hms, "星期：", week)
+    return time_ymd, time_hms, week
     # print(type(time_ymd)) ,"str"
 
 
 def post_data_old():
-    print("正在检索本地数据并发送")
+    print("正在检索本地数据并发送至云服务器")
     db = sqlite3.connect('data.db')
     cursor = db.cursor()
     cursor.execute("select * from speedtest")
@@ -29,11 +37,15 @@ def post_data_old():
     if datas:
         for data in datas:
             try:
+                print(f"正在向服务器发送数据{data}")
+                post_data_to_web(get_speed=(data[1], data[2]), get_time=(data[3], data[4], data[5]), place=data[6])
                 #  发送post到web服务器,删除本地的此条记录
-                cursor.execute("delete from speedtest where id=?", (data[0]))
+                cursor.execute("delete from speedtest where id=?", (data[0],))
                 db.commit()
-            except:
-                pass
+                print(f"从本地删除数据{data}")
+            except Exception as e:
+                error(e)
+
     cursor.close()
     db.close()
 
@@ -56,35 +68,53 @@ def get_speed():
     st = speedtest.Speedtest()
     speed_up = st.upload()
     speed_up = speed_up / 1024 / 1024
-    print("upload:", speed_up, "Mb")
+    print("upload:", speed_up, "Mb/s")
     speed_down = st.download()
     speed_down = speed_down / 1024 / 1024
-    print("download", speed_down, "Mb")
+    print("download", speed_down, "Mb/s")
+    print("测速完毕")
     return speed_up, speed_down
 
 
-def post_data_to_web():
-    speed_up, speed_down = get_speed()
-    time_ymd, time_hms, weeks = get_time()
-    ua = UserAgent()
-    # ua.random随机生成一个User-Agent头部信息
-    header = {'User-Agent': ua.random}
-    url = 'xxx'
-    data_dict = {}
-    response = requests.post(url, headers=header, data=data_dict)
+def post_data_to_web(get_speed, get_time, place=place):
+    print("正在连接云服务器")
+    speed_up, speed_down = get_speed
+    time_ymd, time_hms, week = get_time
+    headers = {
+        'Content-Type': 'application/json',  # 设置 Content-Type 为 JSON 格式
+        'Authorization': 'Bearer 7e958935580e724a73fcb2691bda3b31a609b7c9feff6ae7d0b02a69368feb78'  # 设置 Authorization 头
+    }
+
+    url = 'https://sqtp.zju.external.foraphe.eu.org/api/upload?k=test1&salt=123'
+    data_json = {"payload": [{
+        "location": place,
+        "upload": speed_up,
+        "download": speed_down,
+        "time": time_ymd + " " +time_hms
+    }]}
+    response = requests.post(url, headers=headers, json=data_json)
     print(response)
 
 
 if __name__ == "__main__":
-    post_data_old()
+    try:
+        post_data_old()
+    except:
+        print("连接云服务器失败，请稍后再尝试")
+    get_time = get_time()
+    try:
+        get_speed = get_speed()
+    except Exception as e:
+        print("链接测速服务器失败，请稍后再尝试")
+        error(e)
 
-try:
-    post_data_to_web()
-except:
-    save_data()
+    try:
+        post_data_to_web(get_speed=get_speed, get_time=get_time, place=place)
+    except Exception as e:
+        print(e, "\n将以本地方式存储！！！")
+        save_data()
+        #  连接web服务器发送post请求，若失败则保存在本地数据库，再次访问时重新发送
+    print("所有进程已经结束，敲回车结束程序")
+    end = input()
+    exit(0)
 
-    #  连接web服务器发送post请求，若失败则保存在本地数据库，再次访问时重新发送
-
-
-while True:
-    pass
